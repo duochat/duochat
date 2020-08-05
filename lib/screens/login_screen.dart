be4be7a999ceda955de65cfafd8b96d4a2c06e88
Nathan Wang <nathan.r.wang@gmail.com>
2020-08-04 23:53:14 -0700

    Initial Commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,165 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:duochat/screens/home_screen.dart';
+import 'package:duochat/screens/onboarding_screen.dart';
+import 'package:duochat/widget/loading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  bool isLoading = false;
+
+  Future<Null> handleSignInWithGoogle(BuildContext context) async {
+    this.setState(() {
+      isLoading = true;
+    });
+
+    try {
+      GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      print(credential);
+
+      FirebaseUser firebaseUser =
+          (await _auth.signInWithCredential(credential)).user;
+
+      if (firebaseUser != null) {
+        // Check is already sign up
+        final QuerySnapshot result = await Firestore.instance
+            .collection('users')
+            .where('id', isEqualTo: firebaseUser.uid)
+            .getDocuments();
+        final List<DocumentSnapshot> documents = result.documents;
+        if (documents.length == 0) {
+          // Update data to server if new user
+          Firestore.instance
+              .collection('users')
+              .document(firebaseUser.uid)
+              .setData({
+            'nickname': firebaseUser.displayName,
+            'photoUrl': firebaseUser.photoUrl,
+            'id': firebaseUser.uid,
+            'createdAt': DateTime.now().millisecondsSinceEpoch.toString(),
+            'finishedOnboarding': false,
+          });
+
+          this.setState(() {
+            isLoading = false;
+          });
+
+          Navigator.pushNamed(context, OnboardingScreen.id);
+        } else {
+          this.setState(() {
+            isLoading = false;
+          });
+
+          if (documents[0].data['finishedOnboarding']) {
+            Navigator.pushNamed(context, HomeScreen.id);
+          } else {
+            Navigator.pushNamed(context, OnboardingScreen.id);
+          }
+        }
+      } else {
+        _handleSignInFail(context, 'Google');
+      }
+    } catch (Exception, stacktrace) {
+      print(stacktrace);
+      _handleSignInFail(context, 'Google');
+    }
+  }
+
+  void _handleSignInFail(BuildContext context, String provider) {
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Sign in with $provider failed'),
+      ),
+    );
+    this.setState(() {
+      isLoading = false;
+    });
+  }
+
+  void handleSignInWithFacebook(BuildContext context) {
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Facebook not implemented yet'),
+      ),
+    );
+  }
+
+  void handleSignInWithApple(BuildContext context) {
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Apple not implemented yet'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Builder(
+        builder: (context) => Stack(
+          children: <Widget>[
+            SafeArea(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 24.0),
+                    SignInButton(
+                      Buttons.Google,
+                      onPressed: () => handleSignInWithGoogle(context),
+                    ),
+                    SizedBox(height: 8.0),
+                    SignInButton(
+                      Buttons.Facebook,
+                      onPressed: () => handleSignInWithFacebook(context),
+                    ),
+                    SizedBox(height: 8.0),
+                    SignInButton(
+                      Buttons.Apple,
+                      onPressed: () => handleSignInWithApple(context),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Loading
+            Positioned(
+              child: isLoading ? const Loading() : Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/loading.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class Loading extends StatelessWidget {
+  const Loading();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+        ),
+      ),
+      color: Colors.black.withOpacity(0.8),
+    );
+  }
+}
