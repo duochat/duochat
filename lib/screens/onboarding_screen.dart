4764953e47c1b8049259e44431be4afa28c0af66
Nathan Wang <nathan.r.wang@gmail.com>
2020-08-05 00:03:08 -0700

    stub for onboarding

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:duochat/screens/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firestore = Firestore.instance;
+FirebaseUser firebaseUser;
 
 class OnboardingScreen extends StatefulWidget {
   static String id = 'onboarding_screen';
@@ -8,8 +14,47 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        firebaseUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void handleFinishedOnboarding() {
+    Firestore.instance
+        .collection('users')
+        .document(firebaseUser.uid)
+        .updateData({
+      'finishedOnboarding': true,
+    });
+    Navigator.pushNamed(context, HomeScreen.id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Onboarding Screen'));
+    return SafeArea(
+      child: Center(
+        child: RaisedButton(
+          onPressed: handleFinishedOnboarding,
+          child: Text('Finish Onboarding'),
+        ),
+      ),
+    );
   }
 }
