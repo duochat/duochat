2cfe36d9c3b4ec88bae048e758cd9b595652a3a1
Nathan Wang <nathan.r.wang@gmail.com>
2020-08-05 19:03:05 -0700

    homepage, chat list

--- lib/models.dart
+++ lib/models.dart
@@ -15,3 +15,36 @@ class UserData {
     );
   }
 }
+
+class Chat {
+  final String name;
+  final String id;
+  final String photoURL;
+  List<ChatMessage> messages;
+
+  Chat({this.name, this.id, this.photoURL, this.messages});
+}
+
+class ChatMessageSender {
+  final String name;
+  final String photoURL;
+  final String id;
+
+  ChatMessageSender({this.name, this.photoURL, this.id});
+}
+
+class ChatMessageReadByUser {
+  final String name;
+  final String id;
+
+  ChatMessageReadByUser({this.name, this.id});
+}
+
+class ChatMessage {
+  final ChatMessageSender sender;
+  final String text;
+  final DateTime timestamp;
+  final List<ChatMessageReadByUser> readBy;
+
+  ChatMessage({this.sender, this.text, this.timestamp, this.readBy});
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:duochat/models.dart';
+import 'package:duochat/widget/chat_list.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +10,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Text('Home Page');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Duochat'),
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20.0,
+          ),
+          ChatList(
+            chats: <Chat>[
+              Chat(
+                name: 'Ian Chen',
+                id: 'uniqueid',
+                photoURL: null,
+                messages: <ChatMessage>[
+                  ChatMessage(
+                    sender: ChatMessageSender(
+                      name: "Ian Chen",
+                      photoURL:
+                          'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-1/p100x100/107569367_699524004113639_5166376271392046689_o.jpg?_nc_cat=102&_nc_sid=dbb9e7&_nc_ohc=Kem28j5_aiAAX91MwvD&_nc_ht=scontent-sjc3-1.xx&_nc_tp=6&oh=e8af1536be6e3299f2f20e897b4e5069&oe=5F52B67C',
+                    ),
+                    text: "Ian Chen invited you to chat!",
+                    readBy: [
+                      ChatMessageReadByUser(name: 'Nathan Wang', id: 'asdf'),
+                    ],
+                  ),
+                ],
+              ),
+              Chat(
+                name: 'MUN Officer Team',
+                id: 'uniqueid',
+                photoURL: 'https://picsum.photos/200',
+                messages: <ChatMessage>[
+                  ChatMessage(
+                    sender: ChatMessageSender(
+                      name: "Parth Asawa",
+                      photoURL: '',
+                    ),
+                    text: "Parth Asawa invited you to chat!",
+                  ),
+                ],
+              ),
+              Chat(
+                name: 'Richard Liu',
+                id: 'uniqueid',
+                photoURL: null,
+                messages: <ChatMessage>[
+                  ChatMessage(
+                    sender: ChatMessageSender(
+                      name: "Richard Liu",
+                      photoURL: 'https://picsum.photos/200',
+                    ),
+                    text: "Richard Liu invited you to chat!",
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
